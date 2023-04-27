8ba5b0e6d4ddd4878d92809f28baa41dafd85c8b
ElitrioKO <Gabrielkomljan5@gmail.com>
2023-04-28 04:49:38 +0900

    Project's Initial Commit

---
+++ www/JS/motor.js
@@ -0,0 +1,29 @@
+var digitado
+var placa
+var proprietarionome
+
+function carregar() {
+    alert("Ola mundo")
+}
+
+localStorage.setItem("EFQ-8H67", "Paulo Eduardo Silva Montier")
+localStorage.setItem("FAC-1723, José Carlos")
+localStorage.setItem("EGT-3614", "Felipe Canarozzo")
+localStorage.setItem("DGJ-7567", "Graciente Henrique")
+
+function exibir() {
+    var exibir
+    var digitado
+    digitado = document.getElementById('coletortexto').value
+    exibir = localStorage.getItem(digitado)
+    alert(exibir)
+}
+
+function cadastrar() {
+   placa = document.getElementById('placa').value
+   proprietarionome = document.getElementById('proprietario').value
+   localStorage.setItem(placa, proprietarionome)
+alert("CADASTROU!")
+alert(localStorage.getItem(placa))
+}
+
